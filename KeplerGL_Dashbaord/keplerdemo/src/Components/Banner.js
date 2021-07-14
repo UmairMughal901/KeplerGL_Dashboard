55b417dc6351c87efca9c90996dfd9977a2180ed
Umair Asghar <umairasghar.mughal@gmail.com>
2021-07-14 11:43:37 +0500

    SHowing data

---
+++ KeplerGL_Dashbaord/keplerdemo/src/Components/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../Style/Bannerstyle.css";
+
+
+function Banner() {
+    return (
+        <div id="Mainmenu">
+            <table id="tbl_2">
+                <tr>
+                    <th><img src="https://lh3.googleusercontent.com/proxy/TaWqX2Kf32SROdaNnvlTz82UJ6kyTTbrkISj6_sF7RhCornp87tjH_2gCGG6dwbILOa2EIs-88FmeW12-Rq3zgqviIRfJ3WTgSX81bIYFFAFSszoEbeV8-D7Dh3hlH5-U8q-PbaV" alt="AgriLogo" width="300" height="60"/></th>
+                    <th></th>
+                </tr>
+                
+            </table>
+        </div>
+    )
+}
+
+export default Banner
